1b03ec3a445e92b7cc6e54873326e4ae859b0655
mcghieb <mcghie.blake@gmail.com>
2024-11-15 20:25:23 -0700

    fix: Assignments Model

--- Models/Assignments.cs
+++ Models/Assignments.cs
@@ -1,19 +1,26 @@
 ﻿namespace Models;
 
-public class Assignment(string classCode, string name, string url, string dueDate)
+public class Assignment(string classCode, string name, string url, DateTime dueDate)
 {
     public string ClassCode { get; } = classCode;
     public string Name { get; } = name;
     public string Url { get; } = url;
-    public string DueDate { get; } = dueDate;
+    public DateTime DueDate { get; } = dueDate;
 }
 
 public class Assignments
 {
-    private List<Assignment> assignments { get; } = [];
+    private List<Assignment> AssignmentsList { get; } = [];
+    private int Count { get; set; }
 
     public void AddAssignment(Assignment assignment)
     {
-        assignments.Add(assignment);
+        AssignmentsList.Add(assignment);
+        Count++;
+    }
+
+    public List<Assignment> GetAssignments()
+    {
+        return AssignmentsList;
     }
 }
