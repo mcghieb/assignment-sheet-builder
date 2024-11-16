466d6c103d8e40fcf0b14db40616cceb50ff9ae3
mcghieb <mcghie.blake@gmail.com>
2024-11-15 19:40:45 -0700

    Assignments model created

---
+++ Models/Assignments.cs
@@ -0,0 +1,19 @@
+﻿namespace Models;
+
+public class Assignment(string classCode, string name, string url, string dueDate)
+{
+    public string ClassCode { get; } = classCode;
+    public string Name { get; } = name;
+    public string Url { get; } = url;
+    public string DueDate { get; } = dueDate;
+}
+
+public class Assignments
+{
+    private List<Assignment> assignments { get; } = [];
+
+    public void AddAssignment(Assignment assignment)
+    {
+        assignments.Add(assignment);
+    }
+}
